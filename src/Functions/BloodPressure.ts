17c73c3668c4162a4776d990fc177479cbee4859
damatoaj <mr.arthurdamato@gmail.com>
2022-11-13 21:44:42 -0800

    Added blood pressure classification function

---
+++ src/Functions/BloodPressure.ts
@@ -0,0 +1,7 @@
+export const bloodPressureClassification = (sbp:number, dbp:number) => {
+    if (sbp < 1 || dbp < 1) throw new Error('Paremeters outside of acceptable range');
+    if (sbp < 120 && dbp < 80) return 'normal';
+    if ((sbp >= 120 && sbp <= 139) || (dbp >= 80 && dbp <= 89)) return 'prehypertension';
+    if ((sbp >= 140 && sbp <= 159) || (dbp >= 90 && dbp <= 99)) return 'stage 1 hypertension';
+    if (sbp >= 160 || dbp >= 100) return 'stage 2 hypertension';
+};
