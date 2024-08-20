6d68fc67c38118dcdebe93327e0b6080495d521a
damatoaj <mr.arthurdamato@gmail.com>
2024-08-19 21:01:27 -0700

    Updated nav bar colors with classes

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,13 +1,20 @@
 import { Link } from 'react-router-dom'
 import { User, UserKeys, Link as L } from '../interfaces'
-import { navLinks } from '../Screens/Variables/navLinks'
-
+import { navLinks, sub } from '../Screens/Variables/navLinks'
+import { useState } from 'react'
 interface NavBarProps {
     u: User
 }
 
 
 const NavBar = ({u} : NavBarProps) => {
+    const [state, setState] = useState({
+        tests: 'false',
+        muscularEndurance: 'false',
+        muscularStrength: 'false',
+        cardiovascularEndurance: 'false'
+    });
+
     const links = navLinks.map((link : L, i: number) => {
         let enabled : Boolean = true;
         link.dependencies.forEach((d : UserKeys)=> {
@@ -26,12 +33,89 @@ const NavBar = ({u} : NavBarProps) => {
             </li>
         )}
     })
+
+   
     return (
-        <header>
-            <menu>
-                {links}
-            </menu>
-        </header>
+        <nav>
+            <ul>
+                <li className='light'>
+                    <Link to='/' >Home</Link>
+                </li>
+                <li className='dark'>
+                    <Link to='/parq'>Par-Q</Link>
+                </li>
+                <li className='light'>
+                    <Link to='/nutrition'>Nutrition</Link>
+                </li>
+                <li className='dark'>
+                    <Link to='/equipment'>Equipment</Link>
+                </li>
+                <li className='subnav'>
+                    <button 
+                        type="button" 
+                        className='link light' 
+                        data-active={state.tests}
+                        onClick={()=> state.tests=== 'false' ? setState({...state, tests : 'true'}) : setState({...state, tests : 'false'})}
+                    >
+                        Tests
+                    </button>
+                    <ul data-active='false'>
+                        <li className='sub-subnav'>
+                            <button 
+                                type="button" 
+                                className='link dark' 
+                                data-active={state.muscularEndurance}
+                                onClick={()=> state.muscularEndurance === 'false' ? setState({...state, muscularEndurance : 'true'}) : setState({...state, muscularEndurance : 'false'})}
+                            >
+                                Muscular Endurance
+                            </button>
+                            <ul>
+                                <li className='light'>
+                                    <Link to='/pushups/instructions'>Pushups</Link>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className='sub-subnav'>
+                            <button 
+                                className='link light' 
+                                type='button'
+                                data-active={state.muscularStrength}
+                                onClick={()=> state.muscularStrength === 'false' ? setState({...state, muscularStrength : 'true'}) : setState({...state, muscularStrength : 'false'})}
+
+                            >
+                                Muscular Strength
+                            </button>
+                            <ul>
+                                <li className='dark'>
+                                    <Link to='/bench-press/instructions'>Bench Press</Link>
+                                </li>
+                                <li className='light'>
+                                    <Link to='/grip-strength/instructions'>Grip Strength</Link>
+                                </li>
+                                <li className='dark'>
+                                    <Link to='/leg-press/instructions'>Leg Press</Link>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className='sub-subnav'>
+                            <button 
+                                type="button" 
+                                className='link dark' 
+                                data-active={state.cardiovascularEndurance}
+                                onClick={()=> state.cardiovascularEndurance === 'false' ? setState({...state, cardiovascularEndurance : 'true'}) : setState({...state, cardiovascularEndurance : 'false'})}
+                            >
+                                Cardiovascular Endurance
+                            </button>
+                            <ul>
+                                <li className='light'>
+                                    <Link to='/step-test/instructions'>McArdle Step Test</Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
--- src/Screens/Variables/navLinks.ts
+++ src/Screens/Variables/navLinks.ts
@@ -2,6 +2,7 @@ import { Link } from "../../interfaces";
 
 export const navLinks : Link [] = [
     {label: 'Home', value:'/', dependencies: []},
+    {label: 'Par-Q', value: '/parq', dependencies: []},
     {label:'Pushups', value:'/pushups/instructions', dependencies: ['age', 'sex']},
     {label:'Nutrition', value:'/nutrition', dependencies: ['age', 'sex', 'currentWeight', 'goalWeight']},
     {label: 'Bench Press', value:'/bench-press/instructions', dependencies: ['age', 'sex', 'currentWeight']},
@@ -10,3 +11,26 @@ export const navLinks : Link [] = [
     {label: 'Equipment', value:'/equipment', dependencies: []},
     {label: 'Step Test', value:'/step-test/instructions', dependencies: ['age', 'sex']}
 ]
+
+export const sub = [{topLabel: 'Tests', sublinks: [
+    {
+        label : 'Muscular Endurance',
+        links : [
+            {label:'Pushups', value:'/pushups/instructions', dependencies: ['age', 'sex']}
+        ]
+    },
+    {
+        label : "Muscular Strength",
+        links: [
+            {label: 'Bench Press', value:'/bench-press/instructions', dependencies: ['age', 'sex', 'currentWeight']},
+            {label: 'Leg Press', value:'/leg-press/instructions', dependencies: ['age', 'sex', 'currentWeight']},
+            {label: 'Grip Strength', value:'/grip-strength/instructions', dependencies: ['age', 'sex', 'currentWeight']}
+        ]
+    },
+    {
+        label : "Cardiovascular Endurance",
+        links : [
+            {label: 'Step Test', value:'/step-test/instructions', dependencies: ['age', 'sex']}
+        ]
+    }
+]}]
