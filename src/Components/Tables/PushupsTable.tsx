5f375dbc52550335455fb586e0f890d0f689d14a
damatoaj <mr.arthurdamato@gmail.com>
2024-09-22 18:44:55 -0700

    added pushup table to the summary

---
+++ src/Components/Tables/PushupsTable.tsx
@@ -0,0 +1,24 @@
+import { useUserContext } from "../../Hooks/useUserContext"
+import { memo } from 'react'
+const PushupsTable = () => {
+    const { state : {user} } = useUserContext()
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan={2}>Pushup Test</th>
+                </tr>
+                <tr>
+                    <th>Category: </th>
+                    <th>{user.pushups?.category ? user.pushups.category : 'No Data'}</th>
+                </tr>
+                <tr>
+                    <th>Repetitions: </th>
+                    <th>{user.pushups?.pushups ? user.pushups.pushups : 'No Data'}</th>
+                </tr>
+            </thead>
+        </table>
+    )
+}
+
+export default memo(PushupsTable)
--- src/Components/Tables/SummaryTable.tsx
+++ src/Components/Tables/SummaryTable.tsx
@@ -1,5 +1,6 @@
 import { useUserContext } from "../../Hooks/useUserContext";
 import StrengthTable from "./StrengthTable";
+import PushupsTable from "./PushupsTable";
 import MacrosTable from "./MacrosTable";
 import MicrosTable from "./MicrosTable";
 
@@ -11,6 +12,7 @@ const SummaryTable = () => {
             <h1>Your Fitness Summary</h1>
             <h2>Muscular Strength & Endurance</h2>
             {(state.user.benchPress || state.user.legPress || state.user.gripStrength) ? <StrengthTable /> : <h3>No Summary Yet</h3>}
+            {state.user.pushups && <PushupsTable />}
             <h2>Macro Nutrition Recommendations</h2>
             {state.user.macros ? <MacrosTable  macros={state.user.macros} /> : <h3>No Summary Yet</h3>}
             <h2>Micro Nutrition Recommendations</h2>
