212d905e72ceea19ad45b01aa68ba2d8c1d068fe
damatoaj <mr.arthurdamato@gmail.com>
2024-12-17 07:57:14 -0800

    update switcher

--- src/Components/MetricSwitcher.tsx
+++ src/Components/MetricSwitcher.tsx
@@ -14,8 +14,8 @@ const MetricSwitcher = () => {
             </select>
             :
             <select name='prefers_metric' onChange={handleChangeMetric}>               
-                <option value="true">Metric</option>
                 <option value="false" defaultChecked={true}>Imperial</option>
+                <option value="true">Metric</option>
             </select>
             }
 
