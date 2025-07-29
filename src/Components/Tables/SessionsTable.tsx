864a30749ccbdb600f8539d50b900c9ad3888e31
damatoaj <mr.arthurdamato@gmail.com>
2025-07-28 23:50:07 -0700

    Updated IndexedDb for error handling and the sessions table

--- src/Components/Tables/SessionsTable.tsx
+++ src/Components/Tables/SessionsTable.tsx
@@ -34,7 +34,7 @@ const SessionsTable = () => {
             alert(err);
             setDbReady(true);
         })
-    }, [])
+    }, [version])
 
     if (sessions.length === 0 && !dbReady) return (
         <Loader />
@@ -43,18 +43,34 @@ const SessionsTable = () => {
     return (
             <table>
                 <thead>
-                    <tr><th colSpan={26}>Previous Sessions On This Browser</th></tr>
+                    <tr><th colSpan={28}>Previous Sessions On This Browser</th></tr>
+                    <tr>
+                        <th colSpan={2}></th>
+                        <th colSpan={4}>Demographics</th>
+                        <th colSpan={4}>Body Composition</th>
+                        <th colSpan={8}>Body Circumferences</th>
+                        <th colSpan={4}>Heart & Cardio Related</th>
+                        <th colSpan={1}>Musc Endurance</th>
+                        <th colSpan={3}>Muscular Strength</th>
+                        <th colSpan={1}>Flexibility</th>
+
+
+                    </tr>
                     <tr>
                         <th>Time Stamp</th>
                         <th>Date (YYYY-MM-DD)</th>
+
                         <th>Name</th>
                         <th>Age (YRS)</th>
                         <th>Sex</th>
+                        <th>Activity Level</th>
+
+
                         {user.prefers_metric ? <th>Height (CM)</th> : <th>Height (IN)</th>}
-                        <th>Blood Pressure</th>
                         {user.prefers_metric ? <th>Current Weight (KG)</th> :<th>Current Weight (LBS)</th>} 
                         {user.prefers_metric ? <th>Goal Weight (KG)</th> : <th>Goal Weight (LBS)</th>}
                         <th>BMI</th>
+
                         {user.prefers_metric ? <th>Abdomen Circumference (CM)</th> : <th>Abdomen Circumference (IN)</th>}
                         {user.prefers_metric ? <th>Arm Circumference (CM)</th> : <th>Arm Circumference (IN)</th>}
                         {user.prefers_metric ? <th>Buttocks Circumference (CM)</th> : <th>Buttocks Circumference (IN)</th>}
@@ -63,10 +79,15 @@ const SessionsTable = () => {
                         {user.prefers_metric ? <th>Hip Circumference (CM)</th> : <th>Hip Circumference (IN)</th>}
                         {user.prefers_metric ? <th>Mid Thigh Circumference (CM)</th> : <th>Midthigh Circumference (IN)</th>}
                         {user.prefers_metric ? <th>Waist Circumference (CM)</th> : <th>Waist Circumference (IN)</th>}
+
+                        <th>Resting Heart Rate</th>
                         <th>Max Heart Rate</th>
-                        <th>Activity Level</th>
+                        <th>Blood Pressure</th>
                         <th>VO2 Max</th>
+
+                        
                         <th>Pushups</th>
+                        
                         {user.prefers_metric ? <th>Bench Press (KG)</th> : <th>Bench Press (LBS)</th> }
                         {user.prefers_metric  ? <th>Leg Press (KG)</th> : <th>Leg Press (LBS)</th>}
                         {user.prefers_metric ? <th>Grip Strength (KG)</th> : <th>Grip Strength (LBS)</th>}
@@ -78,14 +99,17 @@ const SessionsTable = () => {
                         return (<tr key={i}>
                             <td>{session.timestamp || '-'}</td>
                             <td>{session.date || '-'}</td>
+
                             <td>{session.name || '-'}</td>
                             <td>{session.age || '-'}</td>
                             <td>{session.sex || '-'}</td>
+                            <td>{session.activity_level || '-'}</td>
+
                             {user.prefers_metric ? <td>{session.height || '-'}</td> : <td>{session.height / 2.54 || '-'}</td>}
-                            <td>{session.systolic_blood_pressure && session.diastolic_blood_pressure ? `${session.systolic_blood_pressure} / ${session.diastolic_blood_pressure}` : '-'}</td>
                             {user.prefers_metric ? <td>{session.current_weight || '-'}</td> : <td>{session.current_weight * 2.2 || '-'}</td>}
                             {user.prefers_metric ? <td>{session.goal_weight || '-'}</td> : <td>{session.goal_weight * 2.2 || '-'}</td>}
                             <td>{session.bmi || '-'}</td>
+
                             {user.prefers_metric ? <td>{session.abdomen_circumference || '-'}</td> : <td>{session.abdomen_circumference / 2.54 || '-'}</td>}
                             {user.prefers_metric ? <td>{session.arm_circumference || '-'}</td> : <td>{session.arm_circumference / 2.54 || '-'}</td>}
                             {user.prefers_metric ? <td>{session.buttocks_circumference || '-'}</td> : <td>{session.buttocks_circumference / 2.54 || '-'}</td>}
@@ -94,14 +118,18 @@ const SessionsTable = () => {
                             {user.prefers_metric ? <td>{session.hips_circumference || '-'}</td> : <td>{session.hips_circumference / 2.54 || '-'}</td>}
                             {user.prefers_metric ? <td>{session.midthigh_circumference || '-'}</td> : <td>{session.midthigh_circumference / 2.54 || '-'}</td>}
                             {user.prefers_metric ? <td>{session.waist_circumference || '-'}</td> : <td>{session.waist_circumference / 2.54 || '-'}</td>}
+
                             <td>{session.resting_heart_rate || '-'}</td>
                             <td>{session.max_heart_rate || '-'}</td>
-                            <td>{session.activity_level || '-'}</td>
+                            <td>{session.systolic_blood_pressure && session.diastolic_blood_pressure ? `${session.systolic_blood_pressure} / ${session.diastolic_blood_pressure}` : '-'}</td>
                             <td>{session.vo2_max || '-'}</td>
+                            
                             <td>{session.pushups || '-'}</td>
+
                             {user.prefers_metric ? <td>{session.bench_press || '-'}</td> : <td>{session.bench_press * 2.2 || '-'}</td>}
                             {user.prefers_metric ? <td>{session.leg_press || '-'}</td> : <td>{session.leg_press * 2.2|| '-'}</td>}
                             {user.prefers_metric ? <td>{session.grip_strength || '-'}</td> : <td>{session.grip_strength * 2.2 || '-'}</td>}
+
                             {user.prefers_metric ? <td>{session.sit_and_reach || '-'}</td> : <td>{session.sit_and_reach / 2.54 || '-'}</td>}
                         </tr>
                     )})}
