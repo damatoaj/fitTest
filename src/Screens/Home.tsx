847b419e756053b670e994c7a018701abcee9c0e
damatoaj <mr.arthurdamato@gmail.com>
2025-08-04 19:34:35 -0700

    Removed border radius on mobile nav button

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => {
                     Whether you're a professional coach, physical therapist, or just curious about your fitness...
                 </p>
                 <p>
-                    FitTest has all of the tests recommended for you!
+                    FitTests has all of the tests recommended for you!
                 </p>
                 <p>
                     To get started, fill out your demographic information. We'll be able to calculate basic health information and provide nutritional recommendations for micro and macro nutrients.
@@ -26,8 +26,36 @@ const Home = () => {
                 <p>
                     The recommended order is: VO2 Max, flexibility, muscular strength, and then end with muscular endurance tests.
                 </p>
+
+                <p>
+                    Feel like you need assistance taking these tests? Try reaching out to a 
+                    <a 
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://www.ecosia.org/search?addon=opensearch&addonversion=7.1.0&q=personal+trainers+near+me'>
+                        personal trainer
+                    </a>,
+                    <a 
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://www.ecosia.org/search?addon=opensearch&addonversion=7.1.0&q=exercise+physiologist+near+me'>
+                        exercise physiologist
+                    </a>,
+                    or
+                    <a 
+                        target='_blank'
+                        rel='noreferrer'
+                        href='https://www.ecosia.org/search?addon=opensearch&addonversion=7.1.0&q=strength+and+conditioning+coach+near+me'>
+                        strength & conditioning coach
+                    </a>
+                </p>
+                <p>
+                    Otherwise, click the link below to start
+                </p>
+                <p>
+                    <Link to='/parq' style={{textAlign:'center'}}>Get Started</Link>
+                </p>
             </article>
-            <Link to='/parq' style={{textAlign:'center'}}>Get Started</Link>
         </main>
     )
 }
