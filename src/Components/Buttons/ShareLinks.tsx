edc0c98628994d61580ba7792b11abfe44218f51
damatoaj <mr.arthurdamato@gmail.com>
2025-12-16 20:55:53 -0800

    updated sms and flex wrap

--- src/Components/Buttons/ShareLinks.tsx
+++ src/Components/Buttons/ShareLinks.tsx
@@ -16,7 +16,7 @@ const links : any = [
     ["mailto:{email_address}?subject={title}&body={url}", faMessage],
     ["https://mail.google.com/mail/?view=cm&to={email_address}&su={title}&body={url}",faGoogle],
     ["http://compose.mail.yahoo.com/?to={email_address}&subject={title}&body={url}", faYahoo],
-    ["sms:{phone_number}?body={url}{text}", faSms],
+    ["sms:''?body={url}\n{title}", faSms],
     ["https://bsky.app/intent/compose?text={url}", faBluesky],
     ['https://threads.net/intent/post?text={url}', faThreads],
     ['https://www.snapchat.com/share?link={url}', faSnapchat]
